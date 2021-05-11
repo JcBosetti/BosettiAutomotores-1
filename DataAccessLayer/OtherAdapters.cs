6f6b20464afc06b1dd01bcde30dab15fb493dadb
Ignazio Martinoli <ignazio.martinoli@gmail.com>
2021-05-10 18:21:29 -0700

    Forgot to build dependencies

--- DataAccessLayer/OtherAdapters.cs
+++ DataAccessLayer/OtherAdapters.cs
@@ -1,24 +1,4 @@
-﻿using System.Data;
-using System.Data.SqlClient;
-
-namespace DataAccessLayer {
+﻿namespace DataAccessLayer {
 	public static class OtherAdapters {
-		public static DataTable LoadGrid(string ip) {
-			using var conn = new SqlConnection(ip);
-			using var cmd = new SqlCommand("SELECT * FROM Empleados", conn);
-
-			conn.Open();
-
-			using var dataAdapter = new SqlDataAdapter(cmd);
-
-			var dataTable = new DataTable();
-			dataAdapter.Fill(dataTable);
-
-			return dataTable;
-		}
-
-		public static void Search(string ip, ) {
-
-		}
 	}
 }
