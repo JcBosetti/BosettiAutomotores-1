ffff55a660b3c10d419cb711305b64fe94c3cb2d
JcBosetti <73072818+JcBosetti@users.noreply.github.com>
2021-05-17 18:20:55 -0300

    Form AddService
    se agrego el formulario de nuevo diagnostico para el service

---
+++ Main/Forms/ServiceForms/addService.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Main.Forms {
+	public partial class AddServices : Form {
+		public AddServices() {
+			InitializeComponent();
+		}
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CancelButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
